2c445a23872771c697f4aa875d5c94fc0d2ea2b1
Addison Mirliani <addisonbm@icloud.com>
2024-12-15 01:48:55 +0100

    AppBar done

--- front_end/src/TreeSelector.js
+++ front_end/src/TreeSelector.js
@@ -1,12 +1,13 @@
 import './TreeSelector.css';
 import React, { useState } from 'react';
 import AppBar from './AppBar';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import TreePreview from './TreePreview';
 
 const TreeSelector = ({ setLoggedIn }) => {
     const exampleTreeData = ["t1", "t2", "t3", "t4"];
     return (
-        <div className='TreeSelector-body'>
+        <div className = 'TreeSelector-body'>
             <AppBar setLoggedIn={setLoggedIn} />
             <div>
                 Your Trees:
@@ -14,9 +15,7 @@ const TreeSelector = ({ setLoggedIn }) => {
             <Row>
                 {exampleTreeData.map((tree, index) => (
                     <Col key={index} sm={12} md={6} lg={4} xl={3}>
-                        <Card>
-                            {tree}
-                        </Card>
+                        <TreePreview/>
                     </Col>
                 ))}
             </Row>
